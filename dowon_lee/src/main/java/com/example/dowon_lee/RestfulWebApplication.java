cde5bc5707100e1593ea0b0193d362a20b77b589
seoin <befine324@gmail.com>
2022-02-04 18:53:41 +0900

    add: ch01

--- dowon_lee/src/main/java/com/example/dowon_lee/RestfulWebApplication.java
+++ dowon_lee/src/main/java/com/example/dowon_lee/RestfulWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DowonLeeApplication {
+public class RestfulWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DowonLeeApplication.class, args);
+        SpringApplication.run(RestfulWebApplication.class, args);
     }
 
 }
---
+++ dowon_lee/src/main/java/com/example/dowon_lee/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.example.dowon_lee.controller;
+
+import com.example.dowon_lee.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String hello() {
+        return "hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello world");
+    }
+
+    @GetMapping("/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+}
