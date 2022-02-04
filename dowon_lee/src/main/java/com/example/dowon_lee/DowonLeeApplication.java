07061f0b081e6eb505954c012c09c6c76582838e
seoin <befine324@gmail.com>
2022-02-04 18:25:36 +0900

    inital commit

---
+++ dowon_lee/src/main/java/com/example/dowon_lee/DowonLeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dowon_lee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowonLeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowonLeeApplication.class, args);
+    }
+
+}
---
+++ dowon_lee/src/test/java/com/example/dowon_lee/DowonLeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dowon_lee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowonLeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
