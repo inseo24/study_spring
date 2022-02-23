784c796cf99ea84cfc599902bc2b0792254fa555
choi seoin <inseo@uupfi.com>
2022-02-23 17:47:29 +0900

    add: jwt 학습

---
+++ apigateway-service/src/main/kotlin/com/example/kotlin/apigatewayservice/config/FilterConfig.kt
@@ -0,0 +1,4 @@
+package com.example.kotlin.apigatewayservice.config
+
+class FilterConfig {
+}
---
+++ 시큐리티/project/jwt/src/main/java/com/example/jwt/service/UserService.java
@@ -0,0 +1,57 @@
+package com.example.jwt.service;
+
+import com.example.jwt.dto.UserDto;
+import com.example.jwt.entity.Authority;
+import com.example.jwt.entity.User;
+import com.example.jwt.repository.UserRepository;
+import com.example.jwt.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null){
+            throw new RuntimeException("이미 가입된 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getUserWithAuthorities(String username){
+        return userRepository.findOneWithAuthoritiesByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> getMyUserWithAuthorities() {
+        return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
+    }
+}
+
